0d1e13fd69147ed93e0236047a13339c0dcd9376
CyanoNoob <geertvandijkg@gmail.com>
2016-04-09 11:52:37 +0200

    mkvendor.sh: Initial commit of gt510wifi

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := gt510wifi
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/gt510wifi/device_gt510wifi.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gt510wifi
+PRODUCT_NAME := cm_gt510wifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := gt510wifi
+PRODUCT_MANUFACTURER := samsung
